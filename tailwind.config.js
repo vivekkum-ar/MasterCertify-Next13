7760cb94b817d81863c38664b2b0c2f852ad29d7
vivekkum-ar <vivekkr9119@gmail.com>
2023-05-26 12:28:40 +0530

    Added Hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{ts,tsx}',
+    './public/**/*.html',
+    './node_modules/flowbite-react/**/*.js',
   ],
   theme: {
     extend: {
@@ -14,5 +17,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 }
