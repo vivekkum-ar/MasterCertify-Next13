71dcec502239971a19cdb3944d55b573a2930d49
vivekkum-ar <vivekkr9119@gmail.com>
2023-06-13 19:14:49 +0530

    Added pixegami's MD Posts

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['bestjquery.com'],
-  },
-  experimental: {
-    mdxRs: true,
-  },
-}
+const nextConfig = {}
 
-const withMDX = require('@next/mdx')()
-module.exports = withMDX(nextConfig)
+module.exports = nextConfig
+module.exports = {
+    images: {
+      domains: ['bestjquery.com'],
+    },
+  }
