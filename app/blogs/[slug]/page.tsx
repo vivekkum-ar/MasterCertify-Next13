5a3e5b81a7756ad2b25237a38bd140f1f25546b8
vivekkum-ar <vivekkr9119@gmail.com>
2023-06-15 20:43:19 +0530

    Props passing BW blogs-[slug]

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
-const page = () => {
+import "@/app/globals.css"
+const page = (props: any) => {
+    const slug = props.params.slug;
   return (
-    <div className='mt-20'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis, recusandae. Voluptatum excepturi totam eius itaque doloribus magni doloremque id in veritatis, enim magnam ut tempora temporibus laboriosam voluptas eveniet possimus iusto accusamus quidem. Animi molestiae cupiditate rerum beatae iusto possimus, maxime incidunt quam, totam architecto tempora facilis consequuntur officia dolore.</div>
+    <div className='mt-20'><h1 className='text-4xl font-bold'>{slug}</h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis, recusandae. Voluptatum excepturi totam eius itaque doloribus magni doloremque id in veritatis, enim magnam ut tempora temporibus laboriosam voluptas eveniet possimus iusto accusamus quidem. Animi molestiae cupiditate rerum beatae iusto possimus, maxime incidunt quam, totam architecto tempora facilis consequuntur officia dolore.</div>
   )
 }
 
